2d771ed1eb1a35436de0688bb25acc1fdddba9ce
Andrii Vlasenko <Andrii.Vlasenko@infiz.khpi.edu.ua>
2025-12-07 22:06:13 +0100

    added comments

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 int main() {
-    int n;  
+    int n;  // кількість чисел фібоначі
     cout << "Enter the amount of Fibonacci numbers (n > 0): ";
     cin >> n;
     if (n <= 0) {
         cout << "must be more then 0." << endl;
-        return 1; 
+        return 1; // повернення з кодом помилки
     }
-    long long first = 0;
-    long long second = 1;
+    long long first = 0; // Перше число
+    long long second = 1; // Пруге число
     long long next;
 
     cout << "first " << n << " Fibonacci numbers:" << endl;
 
+    // виведення перших двох чисел
     if (n >= 1) {
         cout << first << " ";
     }
@@ -23,16 +24,18 @@ int main() {
         cout << second << " ";
     }
 
+    // цикл для решти чисел
     for (int i = 3; i <= n; i++) {
-        next = first + second;
+        next = first + second; // наступне число
 
         cout << next << " ";
 
+        // оновлння змінних
         first = second;
         second = next;
     }
 
-    cout << endl; 
+    cout << endl;
 
     return 0;
 }
